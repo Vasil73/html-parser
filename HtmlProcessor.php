0903aa7e5526763b227e70e65a1503c2e4d3461b
vasily <vs300259@gmail.com>
2023-02-12 07:18:58 +0300

    add files

--- HtmlProcessor.php
+++ HtmlProcessor.php
@@ -2,21 +2,18 @@
 
 
     $json = file_get_contents('php://input');
-    $object = json_decode($json);
-
-    if (isset($object)) {
+    if ($object = json_decode($json)) {
         $object = $object->raw_text;
     } else {
         http_response_code(500);
+        exit;
     }
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $regex = '/<a[^>]*?>(.*?)<\/a>/si';  // /<a [^>]*href="(.*)"[^>]*>(.*)<\/a>/
-        $str = "";
-        $formatted_text = preg_match_all($object, $str, $regex);
+        $formatted_text = preg_match_all($object, '$1', $regex);
         $json = ['formatted_text' => $formatted_text];
         header('Content-Type: application/json');
         echo json_encode($json);
-
     }
 
